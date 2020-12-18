6f0006cf0979bd512326af904e2fdb22e32affa5
Stanislav Kryzhanovsky <stanislavkryzhanovsky@gmail.com>
2020-12-18 13:27:11 +0300

    Some config fixes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,23 @@
 /*jslint node: true, nomen: true*/
 "use strict";
 module.exports = function (logger, config) {
-	var funcs = {};
-	
-	funcs.getUsers = function (cb) {
-		var userlist = ['Alice', 'Bob', 'Charlie'];
-		logger.info('Returning user list');
-		cb(null, userlist);
-	};
-	
-	return funcs;
+  var funcs = {};
+  var userlist = ["Alice", "Bob", "Charlie"];
+
+  funcs.getUsers = function (callback) {
+    logger.info("Returning user list");
+    callback(null, userlist);
+  };
+
+  funcs.getUser = function (id, callback) {
+    logger.info("Returning user ", id);
+    if (userlist.length >= parseInt(id) && parseInt(id) - 1 >= 0) {
+      callback(null, userlist[id - 1]);
+      return;
+    }
+    logger.info("Returning user ", id, " error");
+    callback({ status: 500, message: "Wrong parameter" }, null);
+    return;
+  };
+  return funcs;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 /*jslint node: true, nomen: true*/
 "use strict";
 
-var express = require('express');
+var express = require("express");
+const path = require("path");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-	res.send({
-		title: 'Express app'
-	});
+const templatePath = path.join(__dirname, "../templates");
+
+router.get("/", function (req, res, next) {
+  res.sendFile(`${templatePath}/index/index.html`);
 });
 
 module.exports = router;
